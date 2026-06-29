585abc2a1ad451e34b032dc0060b458870782da4
jasoneseus <jason.clark@student.uva.nl>
2026-06-29 17:00:22 +0200

    execuptext

--- js/scriptss.js
+++
@@ -1,9 +0,0 @@
-
-const showImage = () => {
-    document.getElementById("first").classList.toggle('hidden');
-    }
-
-
-const showImage1 = () => {
-    document.getElementById("second").classList.toggle('hidden');
-    }
